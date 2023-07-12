e9c4213430a8511d44bac25a3dde966539aa136a
Keenen <keenen@Zipcoders-MacBook-Pro.local>
2023-07-12 10:18:25 -0400

    finished WriteIfs portion

--- src/java/WriteIFs.java
+++ src/java/WriteIFs.java
@@ -9,35 +9,55 @@ public class WriteIFs
 {
  
     public void playerDied(boolean player1) {
+        if (!isAlive(player1)) {
+            displayGameOver(player1);
+        }
         // Write an IF statement that checks “player1.isAlive()” 
         // and if that’s false, calls “displayGameOver(player1)”
      
     }
     
-    public String thermoSTAT(int room) {
-        // Write an IF statement that checks the 
+    public String thermoSTAT(int roomTemperature) {
+        if (roomTemperature < 70) {
+            heatOn();
+            return "heating";
+        } else {
+            coolOn();
+            return "cooling";
+        }
+    }
+
+
+        // Write an IF statement that checks the
         // “temperature(room)” and if that check is less than 70, 
         // calls “heatOn()” else calls “coolOn()”
 
 
-        
-        return this.ss;
-    }
+
+
 
     public void fireplaceControl(Object fireplace1) {
+        if (outsideTemp() < 50 && insideTemp() < 62) {
+            startAFire(fireplace1);
+        }
+    }
         // Write an IF statement that checks 
         // “outsideTemp()” is less than 50 
         // AND 
         // “insideTemp()” is less than 62, 
         // calls “startAFire(fireplace1)”
 
-    }
+
 
     public void checkFuel(double fuelLevel) {
+        if (fuelLevel < 0.08) {
+            refuel();
+        }
+    }
         // Write an IF statement that checks “fuelLevel” 
         // and if that check is less than 0.08, calls “refuel()”
 
-    }
+
 
 
     
